f568e43437f8caa98bb331fdf93f46bc0e949c06
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-05-18 22:29:48 -0400

    testing DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,29 @@
+import classNames from 'classnames';
 import React, { useState } from 'react'
+import 'components/DayListItem.scss';
+
+  const formatSpots = (spots) => {
+    if (spots === 1) {
+      return "1 spot remaining";
+    } else if (spots === 0) {
+      return "no spots remaining";
+    } else if (spots > 1) {
+      return `${spots} spots remaining`;
+    }
+  };
 
 export default function DayListItem(props) {
-  // const [day, setDay] = useState;
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} sports remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} </h3>
     </li>
   );
+  
 }
