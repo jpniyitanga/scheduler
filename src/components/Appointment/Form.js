fa0efb8dc1f6076c7da8c65a833d3468ec99f7f5
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-05-22 13:34:58 -0400

    Props and State in the Appointment Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,33 +1,45 @@
-import React from 'react'
-import Button from 'components/Button'
-import InterviewerList from 'components/InterviewerList'
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  const reset = () => {
+    setStudent("");
+    setInterviewer("");    
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
-      />
-    </form>
-    <InterviewerList 
-      /* your code goes here */
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger {/* your code goes here */}>Cancel</Button>
-      <Button confirm {/* your code goes here */}>Save</Button>
-    </section>
-  </section>
-</main>
-  )
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={ event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="student"
+            value={student}
+            type="text"
+            placeholder={props.student ? props.student : "Enter Student Name"}
+            onChange={(e) => setStudent(e.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={(e) => setInterviewer(e)}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}> Cancel </Button>
+          <Button confirm onClick={props.onSave}> Save </Button>
+        </section>
+      </section>
+    </main>
+  );
 }
