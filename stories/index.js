a96af2490ba614782f275ee38c738dfd12d12014
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-05-22 11:00:43 -0400

    Error component + story

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -166,4 +167,5 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
-  .add ("Status", ()=><Status message="Deleteting"/>)
+  .add("Status", () => <Status message="Deleteting" />)
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
