9a87e62e2e2d7f3798dabdbd415daa00dcb32096
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-05-28 19:31:48 -0400

    Implement back function

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+export default function useVisualMode(initial) { 
+  const [mode, setMode] = useState(initial);   
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode) => { 
+    setMode(newMode)
+    setHistory([...history, newMode])
+  }
+
+  const back = () => {         
+    if (history.length > 1) { 
+      history.pop();  
+      setHistory(history)
+      setMode(history[history.length - 1]);      
+    }
+  }
+
+  return { mode, transition, back };
+}
+
+
+
